efb59d375f9400b1f695df3d05bcc9e17fb6ddb7
Saksham093 <m.saksham093@gmail.com>
2022-11-03 20:50:21 +0530

    first commit

---
+++ 03_Rust_Lang_Program/if_else_statement/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let n = 50;
+
+    if n < 50 {
+        println!("The Number is Less Then 50.");
+    }
+    else if n == 50{
+        println!("The Number is Equal to 50.")
+    }
+    else {
+        println!("The Number is Greater Then 50");
+    }
+}
